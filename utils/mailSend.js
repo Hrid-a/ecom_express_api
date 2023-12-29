8899786e0bdbd9d0dc41b213867662596f461a19
Ahmed Hrid <ahmedhridw@gmail.com>
2023-12-29 16:15:39 +0100

    fix bugs

--- utils/mailSend.js
+++ utils/mailSend.js
@@ -5,7 +5,7 @@ const mailSend = async ({ email, subject, message }) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT_KEY,
-        secure: true,
+        // secure: true,
         auth: {
             user: process.env.SMTP_AUTH_USER,
             pass: process.env.SMTP_AUTH_PASS,
